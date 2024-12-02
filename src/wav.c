2ec8e921b6ceca64be27952d9db5df0047ec623a
turly221 <feifan@scntist.com>
2024-12-02 07:11:02 +0000

    commit patch 24292898

--- src/wav.c
+++ src/wav.c
@@ -1295,6 +1295,12 @@ static int wavwritehdr(ft_t ft, int second_header)
         return SOX_EOF;
     }
 
+    if (ft->signal.channels > UINT16_MAX) {
+        lsx_fail_errno(ft, SOX_EOF, "Too many channels (%u)",
+                       ft->signal.channels);
+        return SOX_EOF;
+    }
+
     dwSamplesPerSecond = ft->signal.rate;
     wChannels = ft->signal.channels;
 
